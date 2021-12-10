dfdfe2f02ecd27118fbf6f159753d3e5c7c80789
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2021-12-10 18:54:46 +0000

    Map Marker dragging

--- app/src/main/java/org/wit/pricecalculator/activities/TaskActivity.kt
+++ app/src/main/java/org/wit/pricecalculator/activities/TaskActivity.kt
@@ -16,7 +16,7 @@ class TaskActivity  : AppCompatActivity() {
     lateinit var app: MainApp
     private lateinit var mapIntentLauncher : ActivityResultLauncher<Intent>
 
-
+    var location = Location(52.245696, -7.139102, 15f)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +31,6 @@ class TaskActivity  : AppCompatActivity() {
         registerMapCallback()
 
         locationButton.setOnClickListener {
-            val location = Location(52.245696, -7.139102, 15f)
             val launcherIntent = Intent(this, MapActivity::class.java)
                 .putExtra("location", location)
             mapIntentLauncher.launch(launcherIntent)
@@ -42,6 +41,17 @@ class TaskActivity  : AppCompatActivity() {
     private fun registerMapCallback() {
         mapIntentLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-            { i("Map Loaded") }
+            { result ->
+                when (result.resultCode) {
+                    RESULT_OK -> {
+                        if (result.data != null) {
+                            i("Got Location ${result.data.toString()}")
+                            location = result.data!!.extras?.getParcelable("location")!!
+                            i("Location == $location")
+                        } // end of if
+                    }
+                    RESULT_CANCELED -> { } else -> { }
+                }
+            }
     }
 }
