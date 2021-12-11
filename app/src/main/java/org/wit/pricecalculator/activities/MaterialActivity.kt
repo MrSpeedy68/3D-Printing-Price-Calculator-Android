af2b1bbb57f99ec0af0cddb41b391a8b27ad3b72
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2021-12-11 21:31:26 +0000

    Adding Strings to Firebase needs fixing with images

--- app/src/main/java/org/wit/pricecalculator/activities/MaterialActivity.kt
+++ app/src/main/java/org/wit/pricecalculator/activities/MaterialActivity.kt
@@ -5,10 +5,15 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.squareup.picasso.Picasso
 import org.wit.pricecalculator.R
 import org.wit.pricecalculator.databinding.ActivityMaterialBinding
@@ -16,10 +21,12 @@ import org.wit.pricecalculator.helpers.showImagePicker
 import org.wit.pricecalculator.main.MainApp
 import org.wit.pricecalculator.models.MaterialsModel
 import timber.log.Timber.i
+import com.google.firebase.ktx.Firebase
 
 
 class MaterialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMaterialBinding
+    private lateinit var database: DatabaseReference
     var material = MaterialsModel()
     lateinit var app: MainApp
 
@@ -58,19 +65,38 @@ class MaterialActivity : AppCompatActivity() {
             material.type = binding.materialType.text.toString()
             material.weight = binding.materialWeight.text.toString().toInt()
             material.price = binding.materialPrice.text.toString().toFloat()
-            if (material.name.isEmpty()) {
-                Snackbar.make(it,R.string.no_title, Snackbar.LENGTH_LONG)
-                    .show()
-            } else {
-                if (edit) {
-                    app.materials.update(material.copy())
-                } else {
-                    app.materials.create(material.copy())
-                }
+
+//            database = FirebaseDatabase.getInstance("https://d-printing-price-calculator-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Materials")
+//            database.child(material.name).setValue(material).addOnSuccessListener {
+
+
+
+            database = Firebase.database("https://d-printing-price-calculator-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Materials")
+            database.child(material.name).setValue(material).addOnSuccessListener {
+
+
+                Toast.makeText(this, "Successfully Saved Material", Toast.LENGTH_SHORT).show()
+
+                setResult(RESULT_OK)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed to Save Material", Toast.LENGTH_SHORT).show()
             }
-            i("add Button Pressed: $material")
-            setResult(RESULT_OK)
-            finish()
+
+//            if (material.name.isEmpty()) {
+//                Snackbar.make(it,R.string.no_title, Snackbar.LENGTH_LONG)
+//                    .show()
+//            } else {
+//                if (edit) {
+//                    app.materials.update(material.copy())
+//                } else {
+//
+//                    app.materials.create(material.copy())
+//                }
+//            }
+//            i("add Button Pressed: $material")
+//            setResult(RESULT_OK)
+//            finish()
         }
 
         binding.chooseImage.setOnClickListener {
